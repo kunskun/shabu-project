040be90e2520f6ac951294476827424d1470c724
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2021-04-14 17:27:01 +0700

    แก้ไขการ query
    แก้ไข query ใน database จาก webpro เป็น shabu ประกอบด้วย table (login , .... เพิ่มเติมภายหลัง)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,8 +6,7 @@ router = express.Router();
 router.get("/", async function (req, res, next) {
   try {
     const [rows, fields] = await pool.query(
-      `SELECT a.*, b.file_path FROM blogs AS a LEFT JOIN 
-      (SELECT * FROM images WHERE main=1) AS b ON a.id = b.blog_id;`
+      `SELECT * from login`
     );
     return res.json(rows);
   } catch (err) {
