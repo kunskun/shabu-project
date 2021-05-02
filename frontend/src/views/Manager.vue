7ce4b07d1b0a74c99483402e8dca393feb51194a
kuns019 <56548175+kuns019@users.noreply.github.com>
2021-05-02 13:42:13 +0700

    update tab logo and manager.vue

--- frontend/src/views/Manager.vue
+++ frontend/src/views/Manager.vue
@@ -11,70 +11,152 @@
           <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'sales' ? false:true}" @click="selectSite = 'sales'">
             <span class="title is-6">Sales</span>
           </div>      
-          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'material' ? false:true}" @click="selectSite = 'material'">
+          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'materials' ? false:true}" @click="selectSite = 'materials'">
             <span class="title is-6">Material</span>
           </div>
           <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'menu' ? false:true}" @click="selectSite = 'menu'">
             <span class="title is-6">Menu</span>
           </div>
-          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'order' ? false:true}" @click="selectSite = 'order'">
+          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'orders' ? false:true}" @click="selectSite = 'orders'">
             <span class="title is-6">Order</span>
           </div>
           <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'supplier' ? false:true}" @click="selectSite = 'supplier'">
             <span class="title is-6">Suplier</span>
           </div>
-          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'employee' ? false:true}" @click="selectSite = 'employee'">
+          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'employees' ? false:true}" @click="selectSite = 'employees'">
             <span class="title is-6">Employees</span>
           </div>
-          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'day' ? false:true}" @click="selectSite = 'day'">
+          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'day_account' ? false:true}" @click="selectSite = 'day_account'">
             <span class="title is-6">Day Account</span>
           </div>
-          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'month' ? false:true}" @click="selectSite = 'month'">
+          <div class="button is-full box my-3 is-rounded is-light" :class="{'light-orange': selectSite === 'month_account' ? false:true}" @click="selectSite = 'month_account'">
             <span class="title is-6">Month Account</span>
           </div>
-          
+          {{blogs}}
         </template>
       </div>
 
+      
       <div class="column is-10 px-4  has-background-light is-rounded">
         <!-- Sales -->
-        <div class="column is-full my-6" v-if="selectSite === 'sales'">
-          <h1 class="title is-4">Sales</h1>
+        <div class="column is-full my-5" v-if="selectSite === 'sales'">
+          <div class="columns">
+            <div class="column is-full">
+              <p class="title is-4 has-text-left">Sales
+              <span style="float: right">
+                <button class="button is-primary" @click="getItems">Add new record</button>
+                <button class="button is-danger ml-3">Delete</button>
+              </span>
+              </p>
+            </div>
+          </div>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
                 <th>ID</th>
                 <th>Date</th>
                 <th>Income</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>1</td>
-                <td>2020-01-01</td>
-                <td>300.00 </td>
+                <td>2021-04-16</td>
+                <td>213.00 </td>
+                <td><i class="fa fa-edit"></i></td>
               </tr>
               <tr>
                 <td>2</td>
-                <td>2020-01-02</td>
-                <td>1000.00 </td>
+                <td>2021-04-17</td>
+                <td>212.00 </td>
+                <td><i class="fa fa-edit"></i></td>
               </tr>
               <tr>
                 <td>3</td>
-                <td>2020-01-06</td>
-                <td>2000.00 </td>
+                <td>2021-04-18</td>
+                <td>302.00 </td>
+                <td><i class="fa fa-edit"></i></td>
               </tr>
               <tr>
                 <td>4</td>
-                <td>2020-01-08</td>
-                <td>2300.00 </td>
+                <td>2021-04-18</td>
+                <td>255.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>2021-04-19</td>
+                <td>315.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>2021-04-19</td>
+                <td>242.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>2021-04-19</td>
+                <td>306.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>2021-04-20</td>
+                <td>318.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>9</td>
+                <td>2021-04-21</td>
+                <td>432.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td>2021-04-21</td>
+                <td>492.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>11</td>
+                <td>2021-04-22</td>
+                <td>145.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>12</td>
+                <td>2021-04-22</td>
+                <td>200.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>13</td>
+                <td>2021-04-23</td>
+                <td>117.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>14</td>
+                <td>2021-04-23</td>
+                <td>215.00 </td>
+                <td><i class="fa fa-edit"></i></td>
+              </tr>
+              <tr>
+                <td>15</td>
+                <td>2021-04-24</td>
+                <td>268.00 </td>
+                <td><i class="fa fa-edit"></i></td>
               </tr>
             </tbody>
             <tfoot>
                 <tr>
                     <th></th>
                     <th></th>
-                    <th>Sum : 5600</th>
+                    <th>Sum : 4073.00</th>
+                    <th></th>
                 </tr>
             </tfoot>
           </table>
@@ -85,8 +167,8 @@
         </div> -->
         
         <!-- Order -->
-        <div class="column is-full my-6" v-if="selectSite === 'order'">
-          <h1 class="title is-4">Order</h1>
+        <div class="column is-full my-5" v-if="selectSite === 'orders'">
+          <h1 class="title is-4 has-text-left">Order</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -102,12 +184,12 @@
                 <td>1</td>
                 <td>2020-01-01</td>
                 <td>Cucumber</td>
-                <td>20</td>
+                <td>19</td>
               </tr>
               <tr>
                 <td>2</td>
                 <td>2020-01-01</td>
-                <td>Chicken</td>
+                <td>chicken</td>
                 <td>6</td>
               </tr>
               <tr>
@@ -121,8 +203,8 @@
         </div>
 
         <!-- Material -->
-        <div class="column is-full my-6" v-if="selectSite === 'material'">
-          <h1 class="title is-4">Material</h1>
+        <div class="column is-full my-5" v-if="selectSite === 'materials'">
+          <h1 class="title is-4 has-text-left">Material</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -157,8 +239,8 @@
         </div>
 
         <!-- Menu -->
-        <div class="column is-full my-6" v-if="selectSite === 'menu'">
-          <h1 class="title is-4">Menu</h1>
+        <div class="column is-full my-5" v-if="selectSite === 'menu'">
+          <h1 class="title is-4 has-text-left">Menu</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -200,8 +282,8 @@
         </div>
 
         <!-- Month Account -->
-        <div class="column is-full mx-1 my-6" v-if="selectSite === 'month'">
-          <h1 class="title is-4">Month Account</h1>
+        <div class="column is-full mx-1 my-5" v-if="selectSite === 'month_account'">
+          <h1 class="title is-4 has-text-left">Month Account</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -237,8 +319,8 @@
         </div>
 
         <!-- Day Account -->
-        <div class="column is-full mx-1 my-6" v-if="selectSite === 'day'">
-          <h1 class="title is-4">Day Account</h1>
+        <div class="column is-full mx-1 my-5" v-if="selectSite === 'day_account'">
+          <h1 class="title is-4 has-text-left">Day Account</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -282,8 +364,8 @@
         </div>
 
         <!-- Suplier -->
-        <div class="column is-full mx-1 my-6" v-if="selectSite === 'supplier'">
-          <h1 class="title is-4">Supplier</h1>
+        <div class="column is-full mx-1 my-5" v-if="selectSite === 'supplier'">
+          <h1 class="title is-4 has-text-left">Supplier</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -309,8 +391,8 @@
         </div>
         
         <!-- Employees -->
-        <div class="column is-full mx-1 my-6" v-if="selectSite === 'employee'">
-          <h1 class="title is-4">Employees</h1>
+        <div class="column is-full mx-1 my-5" v-if="selectSite === 'employees'">
+          <h1 class="title is-4 has-text-left">Employees</h1>
           <table class="table is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -345,14 +427,35 @@
         
       </div>
     </div>
+    
   </section>
 </template>
 <script>
+import axios from "axios";
+
 export default {
+  name: 'manager',
   data() {
     return {
       selectSite: 'sales',
+      blogs: [],
     };
+  },
+  mounted() {
+    this.getItems();
+  },
+  methods: {
+    getItems() {
+      axios
+        .get("http://localhost:3000/manager/"+this.selectSite, {test: 'It\'s OK!!'})
+        // .then((response) => {
+        //   console.log(response);
+        //   this.blogs = response.data;
+        // })
+        // .catch((err) => {
+        //   console.log(err);
+        // });
+    },
   },
 };
 </script>
